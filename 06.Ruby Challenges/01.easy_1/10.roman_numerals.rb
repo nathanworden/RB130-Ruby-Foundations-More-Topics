41cd091d1517f67874f32e7ccd99d5c1626ca5d4
Nathan Worden <worden.nathan@gmail.com>
2019-09-30 05:37:34 -0700

    Roman Numerals

--- 06.Ruby Challenges/01.easy_1/10.roman_numerals.rb
+++ 06.Ruby Challenges/01.easy_1/10.roman_numerals.rb
@@ -1,8 +1,9 @@
 # If a number is between 1 and 3, just repeat it
 # If a number is 4 then take the next number up minus 1
+
 require 'pry'
 
-NUMERALS = {'0' => '',
+NUMERALS = {
             '1' => 'I', 
             '4' => 'IV', 
             '5' => 'V', 
@@ -28,24 +29,30 @@ class Integer
     else
       ranges = places.dup
       ranges.map do |num|
-        binding.pry
+        # binding.pry
         if NUMERALS.include?(num)
           NUMERALS[num]
         elsif other_keys_add_up_to(num)
           which_keys_add_up_to?(num)
         else
           transform = ''
-          NUMERALS.keys.reverse.each do |numeral|
-            if num.to_i > numeral.to_i
-              num[0].to_i.times {|tr| transform << NUMERALS[numeral]}
-              break
+          constructors = NUMERALS.dup.delete_if {|key| key.include?('4') || key.include?('9')}
+          constructors.keys.reverse.each do |numeral|
+            if num.to_i >= numeral.to_i
+              if numeral == '1'
+                num[0].to_i.times {|tr| transform << NUMERALS[numeral]}
+                break
+              else
+                transform << NUMERALS[numeral]
+                num = (num.to_i - numeral.to_i).to_s
+              end
             else
               next
             end
           end
           transform
         end
-      end#.join
+      end.join
     end
   end
 
@@ -67,24 +74,39 @@ class Integer
   end
 
   def other_keys_add_up_to(num)
-    keys_except_1 = NUMERALS.dup.delete_if {|key, value| key == '0' || key == '1'}
+    keys_except_1 = NUMERALS.dup.delete_if do |key, value| 
+      key == '0' || 
+      key == '1' ||
+      key.include?('4') ||
+      key.include?('9')
+    end
+    yep = false
     keys_except_1.keys.reverse.each do |numeral|
       if num.to_i > numeral.to_i
         difference = num.to_i - numeral.to_i
-        keys_except_1.include?(difference.to_s)
+        yep = true if keys_except_1.include?(difference.to_s)
+        yep = true if num.to_i % numeral.to_i == 0
       else
         next
       end
     end
+    yep
   end
 
   def which_keys_add_up_to?(num)
     transform = ''
     NUMERALS.keys.reverse.each do |numeral|
       if num.to_i > numeral.to_i
+        if num.to_i % numeral.to_i == 0
+          this_many = num.to_i / numeral.to_i
+          this_many.times {|_| transform << NUMERALS[numeral]}
+          break
+        end
+        transform << NUMERALS[numeral]
         difference = (num.to_i - numeral.to_i).to_s
-        next if !NUMERALS.include?(difference)
-        transform = "#{NUMERALS[numeral]}#{NUMERALS[difference]}"
+        num = difference
+        next if !NUMERALS.include?(num)
+        transform << NUMERALS[difference]
         break
       else
         next
@@ -95,7 +117,19 @@ class Integer
 
 end
 
-p 163.to_roman
+# p 575.to_roman
+# DLXXV
+
+# p 3000.to_roman
+# MMM
+
+# p 6.to_roman
+# VI
+
+# p 48.to_roman
+# 'XLVIII'
+
+# p 163.to_roman
 # CLXIII
 
 # p 160.to_roman
---
+++ 06.Ruby Challenges/01.easy_1/10.roman_numerals_try2.rb
@@ -0,0 +1,6 @@
+# Accept a numer.
+# Transform to expanded notation
+# Transform each number:
+    # Does it have a 1 or 5 in it?
+        # 
+    # 
